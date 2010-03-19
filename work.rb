ea5fcbbc48c68dbb4ba0c664ca940fa01fc75831
Ryukzak Neskazov <ryukzak@gmail.com>
2010-03-19 17:56:09 +0300

    New TODO
    Some edit in work.

--- work.rb
+++ work.rb
@@ -11,25 +11,14 @@ ast = Parser.new( Lexer.run "2 + 2 - pi + 2*4/2" ).run
 opt = Optimizer.new ast
 opt.ast.to_gv_file "out1.gv", "out1.png"
 
-boxing = ->(f) do 
-  puts f.function.name
-  if f.function.name == "-" 
-    puts "boxing"
-    [f.args[0], FunctionApplication.new(->( x ){ - x[ 0 ] }, [f.args[1]])]
-  else f 
-  end
-end
-
-# puts opt.ast.get_operator_line(["+", "-"], boxing)
-
 opt.substitution!
 opt.ast.to_gv_file "out2.gv", "out2.png"
 
-opt.part_eval!
+opt.sort_function!
 opt.ast.to_gv_file "out3.gv", "out3.png"
+
+opt.part_eval!
+opt.ast.to_gv_file "out4.gv", "out4.png"
 
 int = Interpreter.new opt.ast
 puts int.eval
-
-
-
