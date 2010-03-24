c15830216dc58dd5f1fb8076e19cc920126732ac
Ryukzak Neskazov <ryukzak@gmail.com>
2010-03-24 15:33:41 +0300

    Ref AST to fun

--- ast_term.rb
+++ ast_term.rb
@@ -17,14 +17,15 @@ class ASTTerm
     nil
   end
 
-  def to_gv_file(gv_file="out.gv", png_file="out.png")    
-    f = File.new(gv_file,  "w")
+  def to_gv_file gv_file="out.gv", png_file="out.png"
+    f = File.new gv_file, "w"
     f.puts "digraph G {"
     f.puts "size = \"3,3\""
-    self.to_gv f
+    self.to_gv! f
     f.puts "}"
     f.close
     self.reset_gv_flag
+    # call other programm
     system "dot -Tpng #{ gv_file } -o #{ png_file }"
   end
 
@@ -35,15 +36,15 @@ end
 class FunctionApplication < ASTTerm
   attr_accessor :function, :args
 
-  def initialize(fun, args)
+  def initialize fun, args
     self.function = fun
     args = args.to_a  unless args.class == Array
     self.args = args  
   end
   
-  def to_gv(f=$>)
-    f.puts self.args.map { |e| "\"#{ function.to_gv f }\" -> \"#{ e.to_gv f }\";" }
-    function.to_gv
+  def to_gv! f=$>
+    f.puts self.args.map { |e| "\"#{ function.to_gv! f }\" -> \"#{ e.to_gv! f }\";" }
+    function.to_gv!
   end
 
   def reset_gv_flag
@@ -65,7 +66,7 @@ class ASTTermLeaf < ASTTerm
     self.gv = true
   end
 
-  def leaf_to_gv(f, str)
+  def leaf_to_gv f, str
     if self.gv
       f.puts "#{ self.object_id } [label = \"#{ str }\"];"
       self.gv = false
@@ -80,12 +81,12 @@ end
 class Named < ASTTermLeaf
   attr_accessor :name
 
-  def initialize(n)
+  def initialize n
     super()
     self.name = n
   end
 
-  def to_gv(f=$>)
+  def to_gv! f=$>
     leaf_to_gv f, self.name
   end
 
@@ -101,7 +102,7 @@ class Constant < ASTTermLeaf
     self.value = v
   end
 
-  def to_gv(f=$>)
+  def to_gv!(f=$>)
     leaf_to_gv f, self.value
   end
   
@@ -112,14 +113,14 @@ end
 class Function < ASTTermLeaf
   attr_accessor :function, :condition, :arity, :name
 
-  def initialize(f, a=1, c=nil)
+  def initialize f, a=1, c=nil
     super()
     self.function = f
     self.arity = a
     self.condition = c
   end
 
-  def to_gv(f=$>)
+  def to_gv! f=$>
     leaf_to_gv f, self.name
   end
 
